390fdcbcc1a9897a4e44f06dcc6887707d6badff
Alexey Savchenko <alexeysavchenko@alexeys-macbook-pro.local>
2018-07-05 20:46:56 +0300

    Initial Commit

---
+++ GenesisTestTask/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GenesisTestTask
+//
+//  Created by Alexey Savchenko on 7/5/18.
+//  Copyright © 2018 Alexey Savchenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
